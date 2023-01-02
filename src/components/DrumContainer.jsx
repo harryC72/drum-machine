b7f3e9bbfaf6af6ed26e4ea1cff8851f16f8f7fe
Harald Carlsten <haraldcarlsten@yahoo.se>
2023-01-02 16:00:04 +0100

    drumcontainer first commit

---
+++ src/components/DrumContainer.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./DrumContainer.css";
+
+// const letters with an array of letters starting with the letters: "Q", "W", "E", "A", "S", "D", "Z", "X", "C"
+
+const sounds = {
+  heater1: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  heater2: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  heater3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  heater4: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  clap: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  openHH: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  kickNHat: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  kick: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  closedHH: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+};
+
+const letters = [
+  { letter: "Q", label: "Heater 1", sound: sounds.heater1 },
+  { letter: "W", label: "Heater 2", sound: sounds.heater2 },
+  { letter: "E", label: "Heater 3", sound: sounds.heater3 },
+  { letter: "A", label: "Heater 4", sound: sounds.heater4 },
+  { letter: "S", label: "Clap", sound: sounds.clap },
+  { letter: "D", label: "Open HH", sound: sounds.openHH },
+  { letter: "Z", label: "Kick n' Hat", sound: sounds.kickNHat },
+  { letter: "X", label: "Kick", sound: sounds.kick },
+  { letter: "C", label: "Closed HH", sound: sounds.closedHH },
+];
+
+const DrumContainer = () => {
+  const [display, setDisplay] = useState("");
+
+  const play = (sound) => {
+    console.log("Sound: ", sound);
+    const audio = new Audio(sound);
+    audio.currentTime = 0;
+    audio.play();
+  };
+
+  return (
+    <div id="drum-machine">
+      <section className="pad-bank">
+        {letters.map((item, index) => {
+          return (
+            <button
+              id={item.label}
+              className="drum-pad"
+              key={item.letter + index}
+              onClick={() => {
+                play(item.sound);
+                setDisplay(item.label);
+              }}
+            >
+              <audio className="clip" id={item.letter} src={item.sound}></audio>
+              {item.letter}
+            </button>
+          );
+        })}
+      </section>
+      <section id="display">{display}</section>
+    </div>
+  );
+};
+
+export default DrumContainer;
